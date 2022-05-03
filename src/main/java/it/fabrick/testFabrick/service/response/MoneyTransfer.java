14170a0b9ff2232053bfad8973bc590eb06ebb69
maurizio.matteucci <104773763+Megoris82@users.noreply.github.com>
2022-05-03 08:57:15 +0200

    Add files via upload

--- src/main/java/it/fabrick/testFabrick/service/response/MoneyTransfer.java
+++ src/main/java/it/fabrick/testFabrick/service/response/MoneyTransfer.java
@@ -7,6 +7,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import it.fabrick.testFabrick.service.entity.Amount;
 import it.fabrick.testFabrick.service.entity.Creditor;
@@ -16,28 +17,49 @@ import it.fabrick.testFabrick.service.entity.Fee;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MoneyTransfer {
-	
+	@JsonProperty("moneyTransferId")	
 	private BigInteger moneyTransferId;
+	@JsonProperty("status")
 	private String status;
+	@JsonProperty("direction")
 	private String direction;
+	@JsonProperty("creditor")
 	private Creditor creditor;
+	@JsonProperty("debtor")
 	private Debtor debtor;
+	@JsonProperty("cro")
 	private String cro;
+	@JsonProperty("uri")
 	private String uri;
+	@JsonProperty("trn")
 	private String trn;
+	@JsonProperty("description")
 	private String description;
+	@JsonProperty("createdDatetime")
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone="GMT")
 	private Date createdDatetime;
+	@JsonProperty("accountedDatetime")
 	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone="GMT")
-	private Date accountedDatetime;  //": "2019-04-10T10:38:56.000+0200",
-	private Date debtorValueDate; //": "2019-04-10",
-	private Date creditorValueDate; // ": "2019-04-10",
+	private Date accountedDatetime; 
+	@JsonProperty("debtorValueDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+	private Date debtorValueDate; 
+	@JsonProperty("creditorValueDate")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+	private Date creditorValueDate;
+	@JsonProperty("amount")
 	private Amount amount;
+	@JsonProperty("isUrgent")
 	private Boolean isUrgent;
+	@JsonProperty("isInstant")
 	private Boolean isInstant;
+	@JsonProperty("feeType")
 	private String feeType;
+	@JsonProperty("feeAccountId")
 	private BigInteger feeAccountId;
+	@JsonProperty("fees")
 	private List<Fee> fees;
+	@JsonProperty("hasTaxRelief")
 	private Boolean hasTaxRelief;
 
 }
